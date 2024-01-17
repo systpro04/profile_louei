5511062f00cf2ae1f193945c8ac1fda38514756e
systpro04 <systemprogrammer04.com>
2024-01-17 18:10:57 +0800

    enhance 4.0

--- public/js/script.js
+++ public/js/script.js
@@ -43,7 +43,7 @@ ScrollReveal({
     // reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 2000
+    delay: 200
 });
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
 ScrollReveal().reveal('.services-container, .hobbies-container, .portfolio-box, .porject-box, .contact-form', {origin: 'bottom'});
